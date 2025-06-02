178f73e2fe0bc2aa3f35c1566bbdf1fa71c3172c
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-06-03 00:32:05 +0530

    sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "../components/FeaturedCourses"
+import WhyChoooseUS from "@/components/WhyChoooseUS";
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
    <FeaturedCourses/>
+   <WhyChoooseUS/>
    </main>
   );
 }
