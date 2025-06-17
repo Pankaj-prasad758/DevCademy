e47a4a917530fcbb3bd8676aeb11aa1b25e5636c
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-06-18 00:35:02 +0530

    Movingcard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "../components/FeaturedCourses"
 import WhyChoooseUS from "@/components/WhyChoooseUS";
+import MovingCards from "@/components/MovingCards";
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
    <FeaturedCourses/>
    <WhyChoooseUS/>
+   <MovingCards/>
    </main>
   );
 }
